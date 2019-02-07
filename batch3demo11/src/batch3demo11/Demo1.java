5ccb09820f1800fe3bd2742040f6da82fe62dae6
arshu147 <arshu253@gmail.com>
2019-02-07 11:55:38 +0530

    demo2 deleted and demo1 edited

--- batch3demo11/src/batch3demo11/Demo1.java
+++ batch3demo11/src/batch3demo11/Demo1.java
@@ -1,9 +1,7 @@
 package batch3demo11;
 
 public class Demo1 {
-	public static void main(String[] args) {
-		System.out.println();
-		
+	public static void main(String[] args) {	
 	}
 
 }
--- batch3demo11/src/batch3demo11/Demo2.java
+++
@@ -1,5 +0,0 @@
-package batch3demo11;
-
-public class Demo2 {
-
-}
